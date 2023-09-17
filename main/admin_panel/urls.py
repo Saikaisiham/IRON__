5fcde06d6882640460dffec0596f7cc638d2c707
Saikaisiham <blackbutlersiham2001@gmail.com>
2023-09-18 00:00:05 +0100

    commit

--- main/admin_panel/urls.py
+++ main/admin_panel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import sellers_forms
+from .views import sellers_forms, details
 
+app_name = 'seller'
 
 urlpatterns = [
-    path('', sellers_forms)
+    path('', sellers_forms),
+    path('detail/<int:seller_id>/', details, name='seller_detail')
 ]
--- main/admin_panel/views.py
+++ main/admin_panel/views.py
@@ -6,3 +6,10 @@ from seller.models import SellerProfile
 def sellers_forms(request):
     sellers = SellerProfile.objects.all()
     return render(request, 'admin_panel.html', {'sellers':sellers})
+
+
+
+def details(requset, seller_id):
+    seller = SellerProfile.objects.get(id=seller_id)
+    return render(requset, 'seller_detail.html', {'seller':seller})
+
